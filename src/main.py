51822873ce274ed251908ccb171ea0ecaf391814
leonardo <leonardo.luize2@gmail.com>
2022-09-19 19:24:56 -0300

    feat: itera sobre um diretório

--- src/main.py
+++ src/main.py
@@ -3,14 +3,15 @@ import os
  
 directory = './dados'
 reader = ReadFile()
- 
-def create_graph_from_directory(directory):
-  for root, dirs, files in os.walk(directory):
-    for dir in dirs:
-      for root2, dirs2, files2 in os.walk(os.path.join(root, dir)):
-        for file in files2:
+
+def read_directory(root, directory):
+  for new_root, new_dirs, new_files in os.walk(os.path.join(root, directory)):
+      if len(new_files) > 0:
+        for file in new_files:
           if file.endswith(".txt"):
-            lines = reader.readFilesLines(os.path.join(root, dir, file))
+            lines = reader.readFilesLines(os.path.join(new_root, file))
+            #adicionar verificação para identificar quem enviou e quem recebeu
+            #depois adicionar ao grafo
             print(lines)
-          
-create_graph_from_directory(directory)
+
+read_directory(os.path.dirname("./dados"), directory)
