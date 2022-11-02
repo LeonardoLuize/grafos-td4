6d85fde9a541850926fa484fb2ea26ba0c3a949e
LeonardoLuize <leonardo.luize2@gmail.com>
2022-11-02 19:59:51 -0300

    feat: estrutura classe de grafo

--- src/scaleFreeGraph.py
+++ src/scaleFreeGraph.py
@@ -2,7 +2,14 @@ from grafo import Grafo
 
 class ScaleFreeGraph:
 
-    graph
+    graph: Grafo
+    length: int
 
-    def __init__(self):
+    def __init__(self, length: int):
         self.graph = Grafo()
+        self.length = length
+
+    def generate_graph(self) -> Grafo:
+
+
+        return self.graph
