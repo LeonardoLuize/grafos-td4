a53fe8c588922cdf73ff1986ca20d9cc23469554
LeonardoLuize <leonardo.luize2@gmail.com>
2022-11-17 21:27:32 -0300

    feat: adiciona menu para seleção das funções

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ reader = ReadFile()
 nw = Network(True)
 
 users_list = reader.readFilesLines("./src/Users.txt")
-graph = nw.generate_scale_graph(max_size=0, max_edges=5, users_list=users_list, random_graph_size=100)
+graph = nw.generate_scale_graph(max_size=1000, max_edges=5, users_list=users_list, random_graph_size=100)
 
 #graph.imprime_lista_adjacencias()
 
@@ -13,29 +13,49 @@ graph = nw.generate_scale_graph(max_size=0, max_edges=5, users_list=users_list, 
 Separar exercícios por comentário
 """
 
-""" 01: Geração por escala livre, info dos dados """
-print("\n-- info --")
-print("total arestas:", graph.total_arestas())
-print("total vertex:", graph.total_vertices())
+while True:
+    print("\n--- Menu ---")
+    print("| 1. info do grafo")
+    print("| 2. Criação Pajek")
+    print("| 4. Quantidade de componentes")
+    print("| 6. DAG")
+    print("| 7. Histograma Graus")
+    print("| 8. Histograma Caminhos")
+    print("| 0. Sair")
 
-""" 02: Pajek"""
-print("\n-- Criação Pajek --")
-print(graph.pajek())
+    selected = int(input("\nSelecione uma opção: "))
 
-""" 04: Quantidade de componentes"""
-print("\n-- Num Componentes --")
-print("Numero de componentes: ", graph.numberComponents())
+    if selected == 0:
+        break
+    elif selected == 1:
+        """ 01: Geração por escala livre, info dos dados """
+        print("\n-- info --")
+        print("total arestas:", graph.total_arestas())
+        print("total vertex:", graph.total_vertices())
+        
+    elif selected == 2:
+        """ 02: Pajek"""
+        print("\n-- Criação Pajek --")
+        print(graph.pajek())
 
-""" 06: Criação da DAG """
-dag_obj = graph.cria_dag()
-print("\n-- DAG --")
-print(f'DAG: { dag_obj["dag"] }')
-print(f'Edges to remove: { dag_obj["remove"] }')
+    elif selected == 4:
+        """ 04: Quantidade de componentes"""
+        print("\n-- Num Componentes --")
+        print("Numero de componentes: ", graph.numberComponents())
 
-""" 07: Histograma Graus """
-print("\n-- Histograma Graus --")
-dag_obj = graph.histogramaGraus()
+    elif selected == 6:
+        """ 06: Criação da DAG """
+        dag_obj = graph.cria_dag()
+        print("\n-- DAG --")
+        print(f'DAG: { dag_obj["dag"] }')
+        print(f'Edges to remove: { dag_obj["remove"] }')
 
-""" 08: Histograma Caminhos """
-print("\n-- Histograma Caminhos --")
-dag_obj = graph.histogramaCaminhos()
+    elif selected == 7:
+        """ 07: Histograma Graus """
+        print("\n-- Histograma Graus --")
+        dag_obj = graph.histogramaGraus()
+
+    elif selected == 8:
+        """ 08: Histograma Caminhos """
+        print("\n-- Histograma Caminhos --")
+        dag_obj = graph.histogramaCaminhos()
